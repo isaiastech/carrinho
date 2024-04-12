8b56a8a098b4859170539174702dafd049526df3
Isaias <isaiasbatistapy@gmail.com>
2024-04-12 12:33:18 -0300

    criando gitignore do carrinho

---
+++ app/helpers/products.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    1 => ['name'=>'product1', 'price'=>2800],
+    2=> ['name'=>'product2', 'price'=>1600],
+    3 => ['name'=>'product3', 'price'=>230],
+    4 => ['name'=>'product4', 'price'=>120],
+    5 => ['name'=>'product5', 'price'=>350],
+    6 => ['name'=>'product6', 'price'=>700]
+];
---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+require '../vendor/autoload.php';
+
+$products = require'../app/helpers/products.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carinho de compras</title>
+</head>
+<body>
+    <div class="container">
+        <ul>
+            <?php foreach ($products as $index => $product): ?>
+               <li><?php echo $product['name'] ?></li>
+            <?php endforeach;?>
+        </ul>
+    </div>
+</body>
+</html>
